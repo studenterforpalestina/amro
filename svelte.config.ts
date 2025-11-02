a76f1c9caeb8a3d1e90c587d67799f5dc606bfa1
Leif Mørstad <leif@moerstad.no>
2025-11-02 13:41:35 +0100

    refactor: renamed config file to ts

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,12 +1,11 @@
 import adapter from 'svelte-adapter-bun';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { Config } from '@sveltejs/kit';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+const config: Config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
