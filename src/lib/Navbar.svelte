6109518e87cbefd44ffa5ace714f42a8a2e2c739
saimnaveediqbal <17saiiqb@gmail.com>
2025-11-10 10:16:14 +0100

    tweaked styling

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -9,72 +9,29 @@
     let dark = false;
 </script>
 
-<style>
-.parent{
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 1rem;
-}
-.right-side{
-    display:flex
-} 
-
-.logo  {
-    width: 64px;
-}
-
-ul {
-    display: flex;
-    list-style-type: none;
-    align-items: center;
-    gap: 1rem;
-    margin: 0px;
-    padding: 0px 0px 0px 1em;
-}
-
-a {
-    display: flex;
-    text-decoration: none;
-    color:black;
-}
-
-.theme-button{
-    background-color: transparent;
-    border: none;
-}
-
-.red-button {
-    padding: 10px;
-    border-radius: 5px;
-    background-color: #C52128;
-    color: white;
-}
-</style>
-
 <nav>
-    <div class="parent">
-        <div class="logo-container">
-            <a href="/" >
-                <img class="logo" src="/images/logo.png" alt="logo" />
-            </a>
-        </div>
-
-        <div class="right-side">
-            <button class="theme-button" on:click={() => (dark = !dark)}>
+    <div class="flex items-center justify-between px-10 py-6">
+        <a href="/">
+            <img class="w-16" src="/images/logo.png" alt="logo" />
+        </a>
+        <ul class="flex list-none items-center gap-10">
+            <button class="bg-transparent border-0 cursor-pointer" on:click={() => (dark = !dark)}>
                 {#if dark}
                   <Moon />
                 {:else}
                   <Sun />
                 {/if}
             </button>
+            {#each data as item}
+                <li>
+                    <a href={item.url} class="flex">
+                        <p class="text-black text-lg">{item.string}</p> </a>
+                </li>
+            {/each}
 
-            <ul>
-                {#each data.navbar as item}
-                  <li><a href="/">{item}</a></li>
-                {/each}
-                  <a class="red-button" href="/">Støtt oss</a>
-            </ul>
-        </div>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLScR_Y7bGd3znYgbsZC6ECHexUsgjfZkyZYIaTvPtE7nsLSukg/viewform" class="px-4 py-3 text-lg rounded-lg bg-[var(--color-red)] hover:bg-[var(--color-dark-red)] transition-colors duration-300">
+                <p class="text-white">Bli medlem</p>
+            </a>
+        </ul>
     </div>
 </nav>
