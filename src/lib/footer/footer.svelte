d58a08ecd318b0de9e3883dabd0122a50724dd07
Benjamin Zubača <hello@bentduso.com>
2025-11-12 12:06:46 +0100

    Refactor footer link classes for better readability

--- src/lib/footer/footer.svelte
+++ src/lib/footer/footer.svelte
@@ -26,7 +26,10 @@
                         ? undefined
                         : "noopener noreferrer"}
                     aria-label={label}
-                    class="inline-flex items-center justify-center w-10 h-10 rounded-md text-gray-700 hover:text-red-600 hover:bg-red-100 transition-colors duration-200"
+                    class="inline-flex items-center justify-center
+                           w-10 h-10 rounded-md
+                           text-gray-700 hover:text-red-600 hover:bg-red-100
+                           transition-colors duration-200"
                 >
                     <Icon />
                 </a>
