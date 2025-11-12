e54c7132d582d00a039efcb48cf4f59c22a82731
saimnaveediqbal <17saiiqb@gmail.com>
2025-11-12 18:39:12 +0100

    Added hover effects to navbar

--- src/lib/navbar/Navbar.svelte
+++ src/lib/navbar/Navbar.svelte
@@ -16,7 +16,7 @@
         </a>
         <ul class="flex list-none items-center gap-10">
             <li>
-                <button class="bg-transparent border-0 cursor-pointer" on:click={() => (dark = !dark)}>
+                <button class="bg-transparent border-0 cursor-pointer px-3 py-2 rounded-md hover:text-red-600 hover:bg-red-100 transition-colors duration-200" on:click={() => (dark = !dark)}>
                     {#if dark}
                         <Moon />
                     {:else}
@@ -26,7 +26,7 @@
             </li>
             {#each data as item}
                 <li>
-                    <a href={item.url} class="flex text-black text-lg ">
+                    <a href={item.url} class="flex text-black text-lg px-3 py-2 rounded-md hover:text-red-600 hover:bg-red-100 transition-colors duration-200">
                         {item.string} 
                     </a>
                 </li>
