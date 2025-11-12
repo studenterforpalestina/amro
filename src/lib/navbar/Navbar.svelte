48f1b08af63c97daa3bdd17a52341195777e78cf
saimnaveediqbal <17saiiqb@gmail.com>
2025-11-12 18:46:30 +0100

    changed indentation for readability

--- src/lib/navbar/Navbar.svelte
+++ src/lib/navbar/Navbar.svelte
@@ -16,7 +16,11 @@
         </a>
         <ul class="flex list-none items-center gap-10">
             <li>
-                <button class="bg-transparent border-0 cursor-pointer px-3 py-2 rounded-md hover:text-red-600 hover:bg-red-100 transition-colors duration-200" on:click={() => (dark = !dark)}>
+                <button 
+                    class="bg-transparent border-0 cursor-pointer px-3 py-2 rounded-md
+                           hover:text-red-600 hover:bg-red-100 active:bg-red-200
+                           transition-colors duration-200"
+                    on:click={() => (dark = !dark)}>
                     {#if dark}
                         <Moon />
                     {:else}
@@ -26,13 +30,21 @@
             </li>
             {#each data as item}
                 <li>
-                    <a href={item.url} class="flex text-black text-lg px-3 py-2 rounded-md hover:text-red-600 hover:bg-red-100 transition-colors duration-200">
+                    <a 
+                        href={item.url} 
+                        class="flex text-black text-lg px-3 py-2 rounded-md 
+                               hover:text-red-600 hover:bg-red-100 active:bg-red-200
+                               transition-colors duration-200">
                         {item.string} 
                     </a>
                 </li>
             {/each}
             <li>
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLScR_Y7bGd3znYgbsZC6ECHexUsgjfZkyZYIaTvPtE7nsLSukg/viewform" class="px-4 py-3 text-lg text-white rounded-lg bg-[var(--color-red)] hover:bg-[var(--color-dark-red)] transition-colors duration-300">
+                <a 
+                    href="https://docs.google.com/forms/d/e/1FAIpQLScR_Y7bGd3znYgbsZC6ECHexUsgjfZkyZYIaTvPtE7nsLSukg/viewform"
+                    class="px-4 py-3 text-lg text-white rounded-lg bg-[var(--color-red)] 
+                           hover:bg-red-800 active:bg-red-900
+                           transition-colors duration-200">
                     Bli medlem
                 </a>
             </li>
