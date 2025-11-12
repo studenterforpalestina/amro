eb32d9cf11dda8456a36de5624214ddd2c6dce47
Benjamin Zubača <hello@bentduso.com>
2025-11-12 17:17:05 +0100

    Fix indentation in `+layout.svelte`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
-import favicon from "$lib/assets/favicon.svg";
-import Footer from "$lib/footer/footer.svelte";
+    import favicon from "$lib/assets/favicon.svg";
+    import Footer from "$lib/footer/footer.svelte";
 
-const { children } = $props();
-import "../app.css";
+    const { children } = $props();
+    import "../app.css";
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="icon" href={favicon} />
 </svelte:head>
 
 <Footer />
 
 {@render children()}
-
