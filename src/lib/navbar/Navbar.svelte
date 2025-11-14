18c507913155c41c71521d2f613980ceb81836ee
Benjamin Zubača <hello@bentduso.com>
2025-11-14 10:48:16 +0100

    Fix CSS variable syntax for `bg-[var(--color-red)]`

--- src/lib/navbar/Navbar.svelte
+++ src/lib/navbar/Navbar.svelte
@@ -16,11 +16,12 @@
         </a>
         <ul class="flex list-none items-center gap-10">
             <li>
-                <button 
+                <button
                     class="bg-transparent border-0 cursor-pointer px-3 py-2 rounded-md
                            hover:text-red-600 hover:bg-red-100 active:bg-red-200
                            transition-colors duration-200"
-                    on:click={() => (dark = !dark)}>
+                    on:click={() => (dark = !dark)}
+                >
                     {#if dark}
                         <Moon />
                     {:else}
@@ -30,21 +31,23 @@
             </li>
             {#each data as item}
                 <li>
-                    <a 
-                        href={item.url} 
-                        class="flex text-black text-lg px-3 py-2 rounded-md 
+                    <a
+                        href={item.url}
+                        class="flex text-black text-lg px-3 py-2 rounded-md
                                hover:text-red-600 hover:bg-red-100 active:bg-red-200
-                               transition-colors duration-200">
-                        {item.string} 
+                               transition-colors duration-200"
+                    >
+                        {item.string}
                     </a>
                 </li>
             {/each}
             <li>
-                <a 
+                <a
                     href="https://docs.google.com/forms/d/e/1FAIpQLScR_Y7bGd3znYgbsZC6ECHexUsgjfZkyZYIaTvPtE7nsLSukg/viewform"
-                    class="px-4 py-3 text-lg text-white rounded-lg bg-[var(--color-red)] 
+                    class="px-4 py-3 text-lg text-white rounded-lg bg-(--color-red)
                            hover:bg-red-800 active:bg-red-900
-                           transition-colors duration-200">
+                           transition-colors duration-200"
+                >
                     Bli medlem
                 </a>
             </li>
