88579d6d26493c6ae4322c70a4bd6d21a977b9bd
Leif Mørstad <leif@moerstad.no>
2025-11-23 14:34:47 +0100

    refactor: format

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,15 +1,8 @@
-import {
-	getLocaleFromNavigator,
-	init,
-	locale,
-	locales,
-	register,
-} from "svelte-i18n";
+import { getLocaleFromNavigator, init, register } from "svelte-i18n";
 
 register("no-NB", () => import("./locales/no-NB.json"));
 register("en-US", () => import("./locales/en-US.json"));
 
-locales.subscribe(console.log);
 init({
 	fallbackLocale: "no-NB",
 	initialLocale: getLocaleFromNavigator(),
