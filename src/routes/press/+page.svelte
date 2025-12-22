44311e01870f3321f48c7855d5a18f4802bce33c
Saim Iqbal <17saiiqb@gmail.com>
2025-12-22 14:02:47 +0100

    styled blog post with dummy posts

---
+++ src/routes/press/+page.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Press</title>
+	<meta name="description" content="Read recent press releases from Students for Palestine" />
+</svelte:head>
+<div class="mx-auto max-w-5xl p-4 font-sans md:p-8">
+	<h1 class="mb-4 text-3xl font-bold text-gray-900 md:text-7xl">Pressemeldinger</h1>
+</div>
+    
