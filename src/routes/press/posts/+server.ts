00a663300696f71ebc910e6fdd6c08bafd9186ea
Saim Iqbal <17saiiqb@gmail.com>
2026-01-11 17:40:18 +0100

    Delete old json handling files

--- src/routes/press/posts/+server.ts
+++
@@ -1,7 +0,0 @@
-import { fetchBlogPosts } from '$lib/server/fetchPress';
-export const GET = async () => {
-	const posts = await fetchBlogPosts();
-	return new Response(JSON.stringify(posts), {
-		headers: { 'Content-Type': 'application/json' }
-	});
-};
