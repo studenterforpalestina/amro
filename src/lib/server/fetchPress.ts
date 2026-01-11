b9b1900e133c384761f533b3bba643616065f61e
Saim Iqbal <17saiiqb@gmail.com>
2026-01-11 18:17:30 +0100

    update sql quert

--- src/lib/server/fetchPress.ts
+++ src/lib/server/fetchPress.ts
@@ -1,5 +1,5 @@
 import { sql } from 'bun';
 
 export const fetchBlogPosts = async () => {
-	return await sql`SELECT * FROM "PressPost" ORDER BY date DESC`.then((results) => results);
+	return await sql`SELECT * FROM "PressPost" ORDER BY date DESC`;
 };
