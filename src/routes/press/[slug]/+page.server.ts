21e1708217f3818bd2f7d367415d719d47052260
Saim Iqbal <17saiiqb@gmail.com>
2026-01-11 17:18:48 +0100

    fix: lint

--- src/routes/press/[slug]/+page.server.ts
+++ src/routes/press/[slug]/+page.server.ts
@@ -5,8 +5,9 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ params }) => {
 	const { slug } = params;
 
-	const pressRelease = (await sql`SELECT title, content, date FROM "PressPost" WHERE slug = ${slug}`)[0]
-
+	const pressRelease = (
+		await sql`SELECT title, content, date FROM "PressPost" WHERE slug = ${slug}`
+	)[0];
 
 	if (!pressRelease) {
 		throw error(404, 'Press release not found');
