b87f5bf05011733d47d194e853b282f000d367ca
Saim Iqbal <17saiiqb@gmail.com>
2026-01-11 17:33:10 +0100

    dark mode styling

--- src/routes/press/+page.svelte
+++ src/routes/press/+page.svelte
@@ -8,28 +8,28 @@
 	<meta name="description" content="Read recent press releases from Students for Palestine" />
 </svelte:head>
 <div class="mx-auto max-w-5xl p-4 font-sans md:p-8">
-	<h1 class="mb-8 text-3xl font-bold text-gray-900 md:text-7xl">Pressemeldinger</h1>
-	<p class="mb-12 text-lg text-gray-800 md:text-xl">
+	<h1 class="mb-8 text-3xl font-bold  md:text-7xl">Pressemeldinger</h1>
+	<p class="mb-12 text-lg  md:text-xl">
 		Her finner du alle våre pressemeldinger. <br />
 		Presse kan kontakte oss på
 		<a
 			href="mailto:styret@studenterforpalestina.no"
-			class="200 text-red-600 underline transition hover:text-red-800"
+			class="200 text-(--color-red) transition hover:text-(--color-dark-red)"
 		>
 			styret@studenterforpalestina.no
 		</a>
 	</p>
 	{#each data.posts as post (post.slug)}
 		<div class="mx-8 mb-8 max-w-2xl md:max-w-3xl">
-			<time class="text-red-600">{post.date}</time>
+			<time class="text-(--color-red)">{post.date}</time>
 			<a
 				href={resolve(`/press/${post.slug}`)}
 				class="mb-6 block cursor-pointer rounded-md
-            text-black transition-colors
-        duration-200 hover:text-red-600 active:bg-red-200"
+             transition-colors
+        duration-200 hover:text-(--color-red) active:bg-(--color-red)/40"
 			>
 				<h2 class="text-3xl font-semibold">{post.title}</h2>
-				<p class="mt-2 text-lg text-gray-800 duration-200 hover:text-gray-500">Les mer</p>
+				<p class="mt-2 text-(--body-text)/60 text-lg duration-200 ">Les mer</p>
 			</a>
 		</div>
 	{/each}
--- src/routes/press/[slug]/+page.svelte
+++ src/routes/press/[slug]/+page.svelte
@@ -5,15 +5,15 @@
 
 <a
 	href={resolve('/press')}
-	class="mx-8 my-4 rounded-md px-3 py-2 text-lg text-nowrap text-black
+	class="mx-8 my-4 rounded-md px-3 py-2 text-lg text-nowrap 
                             underline transition-colors duration-200
-                            hover:bg-red-100 hover:text-red-600 active:bg-red-200"
+                            hover:bg-(--color-red)/10 hover:text-(--color-red) active:bg-(--color-red)/40"
 >
 	← Alle pressemeldinger
 </a>
 
 <article class="mx-8 my-8 md:mx-auto md:max-w-3xl">
-	<time class="text-red-700">{data.date}</time>
-	<h1 class="mb-4 text-2xl font-bold text-gray-900 md:text-3xl">{data.title}</h1>
-	<p class="text-md whitespace-pre-line text-gray-800 md:text-lg">{data.content}</p>
+	<time class="text-(--color-red)">{data.date}</time>
+	<h1 class="mb-4 text-2xl font-bold md:text-3xl">{data.title}</h1>
+	<p class="text-md whitespace-pre-line md:text-lg">{data.content}</p>
 </article>
