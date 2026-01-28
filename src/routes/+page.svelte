3e339c7c25036b045d8cf5d1b13004ff946ef627
Saim Iqbal <17saiiqb@gmail.com>
2026-01-28 23:23:02 +0100

    tweaked styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,34 @@
 <script>
+    import { _ } from "svelte-i18n";
     import BecomeAMember from "$lib/components/become-a-member.svelte";
     import Section from "$lib/components/section.svelte";
 </script>
 
-<Section fullHeight class="flex flex-col">
+<Section class="flex flex-col items-start">
   <div class="flex flex-col items-start">
-    <h1 class="">Bli med i Studenter</h1>
+    <h1 class="text-8xl font-bold">{$_('components.homepage.join')}</h1>
     <div class="flex gap-16 flex-col xl:flex-row">
       <div class="flex flex-col gap-8">
-        <h1 class="md:text-nowrap">for Palestina</h1>
+        <h1 class="text-8xl font-bold md:text-nowrap">{$_('components.homepage.for_palestina')}</h1>
         <BecomeAMember size="large" class="ml-4" />
       </div>
       <div class="w-full self-end mt-8">
         <img src="/images/demo.png" alt="Demonstrasjon på Trondheims Torg" class="w-full h-auto grow-0 basis-7/12">
       </div>
-
     </div>
   </div>
 </Section>
-<Section red class="flex flex-col lg:flex-row justify-between items-center gap-4">
-  <h2>Meld deg på nyhetsbrevet</h2>
-  <form class="flex gap-2 flex-col items-center max-w-[450px] text-center lg:text-left lg:mt-6">
-    <input type="email" id="email" name="email" placeholder="E-postadresse" required autocomplete="home email" class="border-b text-center max-w-64 transition-all mb-[2.5px] outline-0 placeholder:opacity-100 border-b-(--color-text-light) focus:border-b-4 focus:mb-0"/>
-    <label for="consent" class="flex gap-4 items-center">
-      <input type="checkbox" id="consent" name="consent" required />
-      <p>
-        Jeg godkjenner at studenter for Palestina kan sende meg mail om arrangementer, kampanjer, osv.
-      </p>
-    </label>
-    <button type="submit" class="px-8 py-2 text-(--color-red) rounded-lg bg-(--color-text-light)
+<Section red class="flex flex-col md:flex-row justify-between items-center gap-4 ">
+  <h2 class="text-5xl font-bold text-center">{$_('components.navbar.events')}</h2>
+  
+    <button class="px-8 py-2 text-(--color-red) rounded-lg bg-(--color-text-light)
         hover:bg-red-800 active:bg-red-900 text-nowrap
-          transition-colors duration-200 font-bold">Send inn</button>
-  </form>
+          transition-colors duration-200 font-bold">{$_('components.homepage.see_events')}</button>
+
 </Section>
 <Section class="flex flex-row gap-12">
   <div class="flex flex-col gap-6 grow-0 lg:basis-5/12">
-    <h2>Om oss</h2>
+    <h2 class="text-5xl font-bold max-w-xl">{$_('components.homepage.about_us')}</h2>
     <p>Studenter for Palestina er en studentorganisasjon dedikert til å øke bevisstheten om det palestinske folkets kamp for frihet, rettferdighet ogselvbestemmelse.</p>
     <p>Vi består av studenter fra ulike studieretninger og bakgrunner som er forent i vår solidaritet med Palestina. Vi arrangerer foredrag, filmvisniger og andre arrangementer for å spre informasjon.</p>
   </div>
