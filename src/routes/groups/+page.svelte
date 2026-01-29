8026f62a341ed4e9758bf5441cb91367c4e01624
Luciana <lufrahage@dritflink.no>
2026-01-30 00:51:15 +0100

    Added info on the committees, only in English for now.

--- src/routes/groups/+page.svelte
+++ src/routes/groups/+page.svelte
@@ -7,6 +7,17 @@
 	<meta name="description" content={$_('page.groups.description')} />
 </svelte:head>
 <div>
-	<h1>{$_('page.groups.header')}</h1>
-	<p> whateverrrr </p>
+	<h1 class="text-5xl font-bold p-8">{$_('page.groups.header')}</h1>
+	<p class="text-xl p-4"> Action Committee </p>
+	<p class="p-4"> The action committee organize and coordinate actions and demonstrations. </p>
+	<p class="text-xl p-4"> Design and Social Media </p>
+	<p class="p-4"> Design and Social Media are responsible for all our graphic designs and all posts on our different platforms LINK TO DIFF PLATFORMS HERE ?? </p>
+	<p class="text-xl p-4"> Events Committee  </p>
+	<p class="p-4"> The events committee are responsible for planning events, both social and cultural. </p>
+	<p class="text-xl p-4"> IT Committee </p>
+	<p class="p-4"> The IT committee work with managing and developing the digital solutions of the organization. </p>
+	<p class="text-xl p-4"> Stand Committee </p>
+	<p class="p-4"> The stand committee are responsible for stands, posters, and recruitment on campus.</p>
+	<p class="text-xl p-4"> Writing Group </p>
+	<p class="p-4"> The writing group are tasked with writing texts, opinion pieces and other works of similar nature  </p>
 </div>
