54fa1040e5f93b6acadd022593a5228fcbf86070
Albert <kakke.no@gmail.com>
2026-02-01 21:06:28 +0100

    functionality to the buttons
    There is a bug where saving changes to a user causes those same fields
    to appear empty the next time the edit is opened

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -51,7 +51,7 @@
 		</div>
 
 		{#if data.members && data.members.length > 0}
-			<ul class=" h-120 overflow-auto divide-y divide-gray-400">
+			<ul class=" h-120 divide-y divide-gray-400 overflow-auto">
 				{#each data.members as member}
 					<MemberRow {member} />
 				{/each}
