6f06e01360016385dc3e7ae3f36e045208bb7eb8
Albert <kakke.no@gmail.com>
2026-02-01 02:28:40 +0100

    simple admin page
    made a new table in postgress database (in migrate script), where users are stored, don't
    know if this is where we will store users. One reason this might be a
    bad idea is if the press database is exposed willy nilly then users will
    also be exposed
    
    made the admin page loaddata be server side, because of security and this is the only way
    I have done it before.
    
    made a simple row components that will store the user info, it is still
    missing the correct svg
    
    should aslo add types for user and member

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,40 +1,61 @@
 <script>
-    import { onMount } from 'svelte';
-    import { _ } from 'svelte-i18n';
-    import { getUser, login } from '$lib/auth/UserManager';
+	import { onMount } from 'svelte';
+	import { _ } from 'svelte-i18n';
+	import { getUser, login } from '$lib/auth/UserManager';
+  import MemberRow from '$lib/components/MemberRow.svelte';
 
-    let user = null;
-    let loading = true;
+	export let data;
 
-    onMount(async () => {
-        try {
-            // Check if the user is already logged in
-            user = await getUser();
-            
-            if (!user) {
-                // If no user session exists, redirect to Authentik
-                await login();
-            } else {
-                // User is authenticated!
-                loading = false;
-            }
-        } catch (err) {
-            console.error("Auth check failed:", err);
-        }
-    });
+	let user = null;
+	let loading = true;
+
+	onMount(async () => {
+		try {
+			user = await getUser();
+			if (!user) {
+				await login();
+			} else {
+				loading = false;
+				console.log('Fetched Members:', data.members);
+			}
+		} catch (err) {
+			console.error('Auth check failed:', err);
+		}
+	});
 </script>
 
 <svelte:head>
-    <title>{$_('Admin page')}</title>
+	<title>{$_('Admin page')}</title>
 </svelte:head>
 
 {#if loading}
-    <div>
-        <p>Verifying permissions...</p>
-    </div>
+	<div>
+		<p>Verifying permissions...</p>
+	</div>
 {:else}
-    <div>
-        <h1>ADMIN PAGE</h1>
-        <p>Logged in as: {user?.profile.preferred_username}</p>
-    </div>
+	<div class="p-5">
+		<div class="flex flex-row items-baseline">
+			<h1 class="text-5xl">ADMIN PAGE</h1>
+			<p class="ml-auto">Logged in as: {user?.profile.preferred_username}</p>
+		</div>
+
+<div class="grid text-white grid-cols-6 bg-(--color-red) rounded-lg font-bold items-center my-5 p-2 text-center">
+    <h3 class="text-xl">Name</h3>
+    <h3 class="text-xl">Email</h3>
+    <h3 class="text-xl">Number</h3>
+    <h3 class="text-xl">Graduation</h3>
+    <h3 class="text-xl">Birthyear</h3>
+    <h3 class="text-xl">Action</h3>
+</div>
+
+		{#if data.members && data.members.length > 0}
+			<ul class="divide-y divide-gray-400">
+				{#each data.members as member}
+          <MemberRow {member} />
+				{/each}
+			</ul>
+		{:else}
+			<p>No members found.</p>
+		{/if}
+	</div>
 {/if}
