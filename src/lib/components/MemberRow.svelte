bd7296ef2abfe255d7e4fcf2c048e9bf65793ae6
Albert <kakke.no@gmail.com>
2026-02-01 19:49:43 +0100

    members are made visible on admin page

--- src/lib/components/MemberRow.svelte
+++ src/lib/components/MemberRow.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
 	export let member: any;
+	import { Trash2 } from '@lucide/svelte';
+	import { SquarePen } from '@lucide/svelte';
+
+  function softDeleteButton() {
+    console.log("LMAO")
+	}
 </script>
 
 <li class="grid grid-cols-6 items-center p-2 text-center">
@@ -9,10 +15,15 @@
 	<span>{member.graduationYear}</span>
 	<span>{member.birthYear}</span>
 
-	<div class="grid grid-cols-2 grid-rows-2 gap-2">
-		<div>Edit</div>
-		<div>svg 1</div>
-		<div>Delete</div>
-		<div>svg 2</div>
+	<div class="gap-auto mx-auto flex">
+		<button on:click={softDeleteButton} 
+      class="p-2 mx-2 rounded-xl hover:bg-(--color-red)/40">
+				<SquarePen/>
+		</button>
+		<button on:click={softDeleteButton} 
+      class="p-2 mx-2 rounded-xl hover:bg-(--color-red)/40">
+				<Trash2/>
+		</button>
 	</div>
 </li>
+
