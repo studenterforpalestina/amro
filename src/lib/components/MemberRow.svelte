b762b2a7d18b737e4a522dc1e30418c186684fbb
Albert <kakke.no@gmail.com>
2026-02-01 02:44:39 +0100

    forgot formatting

--- src/lib/components/MemberRow.svelte
+++ src/lib/components/MemberRow.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-    export let member: any;
+	export let member: any;
 </script>
 
-<li class="grid grid-cols-6 items-center p-2 text-center ">
-    <span>{member.name}</span>
-    <span>{member.email}</span>
-    <span>{member.phoneNumber}</span>
-    <span>{member.graduationYear}</span>
-    <span>{member.birthYear}</span>
-    
-    <div class="grid gap-2 grid-cols-2 grid-rows-2">
-    <div>Edit</div>
-    <div>svg 1</div>
-    <div>Delete</div>
-    <div>svg 2</div>
-    </div>
+<li class="grid grid-cols-6 items-center p-2 text-center">
+	<span>{member.name}</span>
+	<span>{member.email}</span>
+	<span>{member.phoneNumber}</span>
+	<span>{member.graduationYear}</span>
+	<span>{member.birthYear}</span>
+
+	<div class="grid grid-cols-2 grid-rows-2 gap-2">
+		<div>Edit</div>
+		<div>svg 1</div>
+		<div>Delete</div>
+		<div>svg 2</div>
+	</div>
 </li>
