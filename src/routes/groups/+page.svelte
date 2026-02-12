e1f5d15942778540e7c0d06da0e226fdd413fe3c
Luciana <lufrahage@dritflink.no>
2026-02-12 19:36:50 +0100

    Change color of committee titles

--- src/routes/groups/+page.svelte
+++ src/routes/groups/+page.svelte
@@ -11,27 +11,27 @@
 	<h1 class="text-5xl font-bold p-8">{$_('page.groups.header')}</h1>
 	<div class="flex gap-4 m-24">
 		<div>
-	<p class="text-xl p-4 bg-lime-800 rounded-xl text-white"> {$_('page.groups.action.title')}</p>
+	<p class="text-xl p-4 bg-green-400 dark:bg-lime-600 rounded-xl text-white"> {$_('page.groups.action.title')}</p>
 	<p class="p-4"> {$_('page.groups.action.description')} </p>
 	</div>
 	<div>
-	<p class="text-xl p-4 bg-lime-800 rounded-xl text-white"> {$_("page.groups.design.title")}</p>
+	<p class="text-xl p-4 bg-green-400 dark:bg-lime-600 rounded-xl text-white"> {$_("page.groups.design.title")}</p>
 	<p class="p-4"> {$_("page.groups.design.description")} </p>
 	</div>
 	<div>
-	<p class="text-xl p-4 bg-lime-800 rounded-xl text-white"> {$_("page.groups.events.title")}  </p>
+	<p class="text-xl p-4 bg-green-400 dark:bg-lime-600 rounded-xl text-white"> {$_("page.groups.events.title")}  </p>
 	<p class="p-4"> {$_("page.groups.events.description")} </p>
 	</div>
 	<div>
-	<p class="text-xl p-4 bg-lime-800 rounded-xl text-white"> {$_("page.groups.it.title")} </p>
+	<p class="text-xl p-4 bg-green-400 dark:bg-lime-600 rounded-xl text-white"> {$_("page.groups.it.title")} </p>
 	<p class="p-4"> {$_("page.groups.it.description")} </p>
 	</div>
 	<div>
-	<p class="text-xl p-4 bg-lime-800 rounded-xl text-white"> {$_("page.groups.stand.title")} </p>
+	<p class="text-xl p-4 bg-green-400 dark:bg-lime-600 rounded-xl text-white"> {$_("page.groups.stand.title")} </p>
 	<p class="p-4"> {$_("page.groups.stand.description")}</p>
 	</div>
 	<div>
-	<p class="text-xl p-4 bg-lime-800 rounded-xl text-white"> {$_("page.groups.writing.title")} </p>
+	<p class="text-xl p-4 bg-green-400 dark:bg-lime-600 rounded-xl text-white"> {$_("page.groups.writing.title")} </p>
 	<p class="p-4"> {$_("page.groups.writing.description")} </p>
 	</div>
 </div>
