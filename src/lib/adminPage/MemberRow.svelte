052f41b9a6d25ba1b7f6c80f98e91212000f869d
Saim Iqbal <17saiiqb@gmail.com>
2026-02-22 13:53:12 +0100

    Tweaked styling of admin page

--- src/lib/adminPage/MemberRow.svelte
+++ src/lib/adminPage/MemberRow.svelte
@@ -8,22 +8,28 @@
 	let editModal: EditMemberModal;
 </script>
 
-<li class="grid grid-cols-6 items-center p-2 text-center">
+<li class="grid grid-cols-[1fr_1fr_1fr_0.8fr_0.8fr_0.5fr] items-center p-2">
 	<span>{member.name}</span>
 	<span>{member.email}</span>
 	<span>{member.phoneNumber}</span>
 	<span>{member.graduationYear}</span>
 	<span>{member.birthYear}</span>
 
-	<div class="gap-auto mx-auto flex">
-		<button on:click={() => editModal.show()} class="mx-2 rounded-xl p-2 hover:bg-(--color-red)/40">
+	<div class=" flex">
+		<button
+			on:click={() => editModal.show()}
+			class="mx-2 rounded-xl p-2 transition-colors duration-200 hover:bg-(--color-red)/40"
+		>
 			<SquarePen />
 		</button>
 
 		<form method="POST" action="?/softDelete" use:enhance>
 			<input type="hidden" name="id" value={member.id} />
 
-			<button type="submit" class="mx-2 rounded-xl p-2 text-(--color-red) hover:bg-red-500/40">
+			<button
+				type="submit"
+				class="mx-2 rounded-xl p-2 text-(--color-red) transition-colors duration-200 hover:bg-(--color-red)/40"
+			>
 				<Trash2 />
 			</button>
 		</form>
