83bf63b6030cad9dfe35eb3dea9e1c69d5596317
Saim Iqbal <17saiiqb@gmail.com>
2026-02-22 13:53:12 +0100

    Tweaked styling of admin page

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -34,17 +34,17 @@
 		<p>Verifying permissions...</p>
 	</div>
 {:else}
-	<div class="p-5">
+	<div class="mx-24 p-4 md:p-8">
 		<div class="flex flex-row items-baseline">
-			<h1 class="text-5xl">ADMIN PAGE</h1>
+			<h1 class="text-5xl font-bold">ADMIN PAGE</h1>
 
-			<div class="ml-auto flex">
+			<div class="ml-auto flex items-center">
 				<p class="mr-3 py-3">Logged in as: {user?.profile.preferred_username}</p>
 
 				<button
 					on:click={logout}
-					class="rounded-lg bg-(--color-red) px-4 py-3 text-lg text-nowrap text-white
-		transition-colors duration-200 hover:bg-red-800 active:bg-red-900"
+					class="max-w-24 rounded-md bg-(--color-red) px-2 py-2 text-sm text-nowrap
+		text-white transition-colors duration-200 hover:bg-red-800 active:bg-red-900"
 				>
 					Logg out
 				</button>
@@ -52,18 +52,18 @@
 		</div>
 
 		<div
-			class="my-5 grid grid-cols-6 items-center rounded-lg bg-(--color-red) p-2 text-center font-bold text-white"
+			class="mt-5 grid grid-cols-[1fr_1fr_1fr_0.8fr_0.8fr_0.5fr] rounded-md bg-(--color-red) p-2 font-bold text-gray-100"
 		>
-			<h3 class="text-xl">Name</h3>
-			<h3 class="text-xl">Email</h3>
-			<h3 class="text-xl">Number</h3>
-			<h3 class="text-xl">Graduation</h3>
-			<h3 class="text-xl">Birthyear</h3>
-			<h3 class="text-xl">Action</h3>
+			<h3 class="">Name</h3>
+			<h3>Email</h3>
+			<h3>Number</h3>
+			<h3>Graduation</h3>
+			<h3>Birthyear</h3>
+			<h3>Action</h3>
 		</div>
 
 		{#if data.members && data.members.length > 0}
-			<ul class=" h-120 divide-y divide-gray-400 overflow-auto">
+			<ul class="divide-y divide-gray-400/20 overflow-auto">
 				{#each data.members as member (member.id)}
 					<MemberRow {member} />
 				{/each}
