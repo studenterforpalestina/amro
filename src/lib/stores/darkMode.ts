01ee30343e7e4fff5542b512b5216e1eade0b68c
Saim Iqbal <17saiiqb@gmail.com>
2026-02-27 18:48:05 +0100

    check darkmode in html

--- src/lib/stores/darkMode.ts
+++ src/lib/stores/darkMode.ts
@@ -2,8 +2,5 @@ import { writable } from 'svelte/store';
 
 const prefersDark =
 	typeof window !== 'undefined' && window.matchMedia('(prefers-color-scheme: dark)').matches;
-if (typeof document !== 'undefined' && prefersDark) {
-	document.documentElement.classList.add('dark');
-}
 
 export const darkMode = writable(prefersDark);
