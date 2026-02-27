25bdd633d0d81097e980d1fb68bd68d46dad13d8
Saim Iqbal <17saiiqb@gmail.com>
2026-02-27 19:21:35 +0100

    preload norwegian locale

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,7 @@
 import { getLocaleFromNavigator, init, register } from 'svelte-i18n';
+import noNB from './locales/no-NB.json';
 
-register('no-NB', () => import('./locales/no-NB.json'));
+register('no-NB', () => Promise.resolve(noNB));
 register('en-US', () => import('./locales/en-US.json'));
 
 init({
