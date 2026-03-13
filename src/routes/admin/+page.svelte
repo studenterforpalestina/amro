d280749d86214666b5cdb7c11f5caa437ac70347
Saim Iqbal <17saiiqb@gmail.com>
2026-03-13 15:32:50 +0100

    Localize admin

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -25,9 +25,9 @@
 		<p>{$_('page.admin.loading')}</p>
 	</div>
 {:else}
-	<div class="mx-24 p-4 md:p-8">
+	<div class="mx-4 p-4 md:mx-24 md:p-8">
 		<div class="flex flex-row items-baseline">
-			<h1 class="text-5xl font-bold">{$_('page.admin.title')}</h1>
+			<h1 class="text-3xl font-bold md:text-5xl">{$_('page.admin.title')}</h1>
 
 			<div class="ml-auto flex items-center">
 				<p class="mr-3 py-3">{$_('page.admin.logged_in')}: {data.user?.preferred_username}</p>
@@ -36,21 +36,30 @@
 					onclick={logout}
 					class="max-w-24 rounded-md bg-(--color-red) px-2 py-2 text-sm text-nowrap
 		text-white transition-colors duration-200 hover:bg-red-800 active:bg-red-900"
+					aria-label="Logout"
 				>
 					{$_('page.admin.logout')}
 				</button>
 			</div>
 		</div>
 
-		<table class="mt-5 w-full table-auto border-collapse border-spacing-20">
+		{#if form?.success && form?.notice}
+			<p
+				class="mt-4 rounded-xl border border-(--color-green) bg-(--color-green)/10 px-4 py-3 text-sm text-(--color-green)"
+			>
+				{$_(form.notice)}
+			</p>
+		{/if}
+
+		<table class="mt-5 w-full table-auto">
 			<thead class="m-200 text-left font-bold text-(--color-red)">
 				<tr>
-					<th scope="col" class="p-2">{$_('page.admin.name')}</th>
-					<th scope="col" class="p-2">{$_('page.admin.email')}</th>
-					<th scope="col" class="p-2">{$_('page.admin.phone_number')}</th>
-					<th scope="col" class="p-2">{$_('page.admin.graduation_year')}</th>
-					<th scope="col" class="p-2">{$_('page.admin.birth_year')}</th>
-					<th scope="col" class="p-2">{$_('page.admin.action')}</th>
+					<th scope="col" class="p-1 md:p-2">{$_('page.admin.name')}</th>
+					<th scope="col" class="p-1 md:p-2">{$_('page.admin.email')}</th>
+					<th scope="col" class="p-1 md:p-2">{$_('page.admin.phone_number')}</th>
+					<th scope="col" class="p-1 md:p-2"> {$_('page.admin.graduation_year')}</th>
+					<th scope="col" class="p-1 md:p-2"> {$_('page.admin.birth_year')}</th>
+					<th scope="col" class="p-1 md:p-2"> {$_('page.admin.action')}</th>
 				</tr>
 			</thead>
 			<tbody>
