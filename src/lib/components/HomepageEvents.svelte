a3bffc8469f604765e89546b5cb9716090e04f15
Saim Iqbal <17saiiqb@gmail.com>
2026-03-19 10:56:51 +0100

    Added fb link to homepage events

--- src/lib/components/HomepageEvents.svelte
+++ src/lib/components/HomepageEvents.svelte
@@ -25,25 +25,30 @@
 {#if events.length > 0}
 	<div class="grid gap-2 md:grid-cols-3 lg:gap-4">
 		{#each events as event (event.id)}
-			<div class="bg-(--color-text-light)/10 p-2 backdrop-blur-sm md:p-4">
-				<img
-					src={event.image_url}
-					alt={event.name}
-					class="mb-4 h-36 w-full rounded-md object-cover md:h-36 lg:h-48"
-					decoding="async"
-					loading="lazy"
-				/>
-				<p class="font-bold text-(--color-text-light)/60 md:text-lg">
-					{getDate(event.start_time)}
-				</p>
-				<h3 class="mt-1 text-lg font-bold md:text-2xl">{event.name}</h3>
-				<div class="mt-3 flex flex-col gap-1 text-sm">
-					<p>🕔 {getTime(event.start_time)}</p>
-					{#if event.place}
-						<p>📍 {event.place}</p>
-					{/if}
+			<a
+				href={`https://www.facebook.com/events/${event.id}`}
+				class="block h-full transition-transform duration-200 hover:scale-[1.01] active:scale-[0.99]"
+			>
+				<div class="h-full bg-(--color-text-light)/10 p-2 backdrop-blur-sm md:p-4">
+					<img
+						src={event.image_url}
+						alt={event.name}
+						class="mb-4 h-36 w-full rounded-md object-cover md:h-36 lg:h-48"
+						decoding="async"
+						loading="lazy"
+					/>
+					<p class="font-bold text-(--color-text-light)/60 md:text-lg">
+						{getDate(event.start_time)}
+					</p>
+					<h3 class="mt-1 text-lg font-bold md:text-2xl">{event.name}</h3>
+					<div class="mt-3 flex flex-col gap-1 text-sm">
+						<p>🕔 {getTime(event.start_time)}</p>
+						{#if event.place}
+							<p>📍 {event.place}</p>
+						{/if}
+					</div>
 				</div>
-			</div>
+			</a>
 		{/each}
 	</div>
 {:else}
