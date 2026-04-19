dda0a66bf16af0e30a06327cf3d17bee125e46d0
Saim Iqbal <17saiiqb@gmail.com>
2026-04-20 00:00:33 +0200

    Tweaked styling of post entry

--- src/lib/components/PostItem.svelte
+++ src/lib/components/PostItem.svelte
@@ -4,19 +4,32 @@
 	import type { Post } from '$lib/types';
 
 	export let post: Post;
+	const dateOptions: Intl.DateTimeFormatOptions = {
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit'
+	};
 </script>
 
-<div class="mx-8 max-w-2xl py-8 md:max-w-3xl">
-	<time class="text-(--color-red)">{new Date(post.date).toLocaleDateString()}</time>
+<div class="mx-8 max-w-2xl py-4 md:max-w-3xl">
+	<time class="text-lg font-bold opacity-60"
+		>{new Date(post.date).toLocaleDateString('no-NB', dateOptions)}</time
+	>
 	<a
 		href={resolve(`/news/${post.slug}`)}
-		class="mb-6 block cursor-pointer rounded-md
+		class="block cursor-pointer rounded-md
              transition-colors
         duration-200 hover:text-(--color-red) active:bg-(--color-red)/40"
 	>
-		<h2 class="text-3xl font-semibold">{post.title}</h2>
+		<h2 class="text-2xl font-semibold md:text-3xl">{post.title}</h2>
 	</a>
-	<p class="text-md whitespace-pre-line md:text-lg">
+	<p class="text-(--color-red) italic">
 		{$_(`page.news.tags.${post.tag}`)}
 	</p>
+	<a
+		href={resolve(`/news/${post.slug}`)}
+		class="transition-colors duration-200 hover:text-(--color-red)/80"
+	>
+		{$_('common.read_more')}
+	</a>
 </div>
