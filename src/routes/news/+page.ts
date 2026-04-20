f3b3428bf734b84099f22ba2d2abe412845df9f0
Saim Iqbal <17saiiqb@gmail.com>
2026-04-20 11:59:26 +0200

    Show new post button when logged in

--- src/routes/news/+page.ts
+++ src/routes/news/+page.ts
@@ -1,7 +1,9 @@
 import type { PageLoad } from './$types';
 import { newsTags, type NewsTag, type Post } from '$lib/types';
+import { error } from '@sveltejs/kit';
 
-export const load: PageLoad = async ({ fetch, url }) => {
+export const load: PageLoad = async ({ fetch, url, parent }) => {
+	const { authorized } = await parent();
 	const tag = url.searchParams.get('tag');
 	const activeFilter: 'none' | NewsTag =
 		tag && newsTags.includes(tag as NewsTag) ? (tag as NewsTag) : 'none';
@@ -9,13 +11,11 @@ export const load: PageLoad = async ({ fetch, url }) => {
 	const endpoint = activeFilter === 'none' ? '/news/posts' : `/news/posts?tag=${activeFilter}`;
 	const response = await fetch(endpoint);
 	if (!response.ok) {
-		return {
-			status: response.status,
-			error: new Error(`Could not load news posts: ${response.statusText}`)
-		};
+		throw error(response.status, `Could not load news posts: ${response.statusText}`);
 	}
 	const posts = (await response.json()) as Post[];
 	return {
+		authorized,
 		posts,
 		activeFilter
 	};
