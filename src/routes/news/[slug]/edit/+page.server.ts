e364307de917addeecd1d0e2537d4900711f9f0b
Saim Iqbal <17saiiqb@gmail.com>
2026-04-20 16:29:47 +0200

    Moved edit form to component

--- src/routes/news/[slug]/edit/+page.server.ts
+++ src/routes/news/[slug]/edit/+page.server.ts
@@ -1,21 +1,50 @@
 import { sql } from 'bun';
 import { error } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, Actions } from './$types';
+import { fail } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const { slug } = params;
 
 	const pressRelease = (
-		await sql`SELECT title, content, date FROM "PressPost" WHERE slug = ${slug}`
+		await sql<Post[]>`
+		SELECT *
+		FROM "PressPost"
+		WHERE slug = ${slug}
+	`
 	)[0];
 
 	if (!pressRelease) {
 		throw error(404, 'Press release not found');
 	}
 
-	return {
-		title: pressRelease.title,
-		content: pressRelease.content,
-		date: pressRelease.date
-	};
+	return { post: pressRelease };
+};
+
+export const actions: Actions = {
+	default: async (event) => {
+		const formData = await event.request.formData();
+		const title = formData.get('title')?.toString();
+		const authors = formData
+			.get('author')
+			?.toString()
+			.split(',')
+			.map((a) => a.trim());
+		const tag = formData.get('tag')?.toString();
+		const content = formData.get('content')?.toString();
+
+		try {
+			await sql`
+				UPDATE "PressPost"
+				SET title = ${title}, authors = ${sql.array(authors)}, tag = ${tag}, content = ${content}, "updatedAt" = now()
+				WHERE slug = ${event.params.slug}
+			`;
+			return { success: true, notice: 'page.news.post_updated' };
+		} catch (err) {
+			console.error(err);
+			return fail(500, {
+				errors: { form: 'common.form.errors.update_failed' }
+			});
+		}
+	}
 };
