03bfc03e5cbe7667a1ada80f70d5d14a8a225d96
Albert <kakke.no@gmail.com>
2026-04-24 10:32:09 +0200

    bug fix and visual feedback

--- src/routes/news/[slug]/edit/+page.svelte
+++ src/routes/news/[slug]/edit/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-	import PostForm from '$lib/components/PostForm.svelte';
-	import type { PageData } from '../$types';
-	let { data }: { data: PageData } = $props();
+    import PostForm from '$lib/components/PostForm.svelte';
+    import type { PageData, ActionData } from './$types';
+    
+    let { data, form }: { data: PageData; form: ActionData } = $props();
 </script>
 
-<PostForm postData={data.post} newPost={false} />
+<PostForm postData={data.post} newPost={false} {form} />
