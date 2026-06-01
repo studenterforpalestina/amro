6d6475ee84c1b79283136f6ecfe522c4c7f60f0d
Saim Iqbal <17saiiqb@gmail.com>
2026-06-01 21:29:32 +0200

    Add standardlink and pagemeta

---
+++ src/lib/components/StandardLink.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { ExtendHTML } from '../../utils/types';
+
+	export type StandardLinkProps = ExtendHTML<
+		'a',
+		{
+			external?: boolean;
+			color?: 'red' | 'green';
+		}
+	>;
+	const { href, children, external, color }: StandardLinkProps = $props();
+</script>
+
+<!-- eslint-disable svelte/no-navigation-without-resolve -->
+<a
+	{href}
+	class="mt-2 block text-lg font-bold text-(--color-{color}) duration-200 hover:opacity-50"
+	rel={external ? 'external noopener noreferrer' : undefined}
+	target={external ? '_blank' : undefined}
+>
+	{@render children?.()}
+</a>
---
+++ src/routes/events/PageMeta.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+	let { pagename }: { pagename: string } = $props();
+</script>
+
+<svelte:head>
+	<title>{$_(`page.${pagename}.title`)}</title>
+	<meta name="description" content={$_(`page.${pagename}.description`)} />
+</svelte:head>
