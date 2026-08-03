f463bd9df6171e38f79edd5d3939fba42990e0fe
Saim Iqbal <17saiiqb@gmail.com>
2026-08-03 20:03:13 +0200

    Added short sumamry to privacy policy and fixed sentence about data handling

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,24 +1,34 @@
 <script>
 	import { _ } from 'svelte-i18n';
+	import StandardParagraph from '$lib/components/common/StandardParagraph.svelte';
+	import PageWrapper from '$lib/components/common/PageWrapper.svelte';
+	import PageHeader from '$lib/components/common/PageHeader.svelte';
+	import PageMeta from '$lib/components/common/PageMeta.svelte';
+	import StandardLink from '$lib/components/common/StandardLink.svelte';
 </script>
 
-<svelte:head>
-	<title>{$_('page.privacy.title')}</title>
-	<meta name="description" content={$_('page.privacy.description')} />
-	<link rel="icon" href="/images/logo.png" />
-</svelte:head>
+<PageMeta pagename="privacy" />
 
-<div class="mx-auto max-w-3xl p-4 font-sans md:max-w-4xl md:p-8">
-	<h1 class="mb-8 text-3xl font-bold md:text-7xl">{$_(`page.privacy.header`)}</h1>
-	<p class="text-md whitespace-pre-line md:text-lg">
+<PageWrapper>
+	<PageHeader>{$_('page.privacy.header')}</PageHeader>
+
+	<StandardParagraph>
 		{$_(`page.privacy.introduction`)}
-	</p>
+	</StandardParagraph>
+	<StandardParagraph>
+		<span class="font-bold">{$_('page.privacy.summary_header')}</span>
+		{$_(`page.privacy.summary`)}
+		<StandardLink href="mailto:styret@studenterforpalestina.no" color="green">
+			styret@studenterforpalestina.no
+		</StandardLink>.
+		{$_(`page.privacy.details_below`)}
+	</StandardParagraph>
 	<h2 class="mt-12 text-2xl font-bold md:text-4xl">
 		{$_(`page.privacy.data_collection`)}
 	</h2>
-	<p class="text-md mt-4 whitespace-pre-line md:text-lg">
+	<StandardParagraph>
 		{$_(`page.privacy.data_collection_details`)}
-	</p>
+	</StandardParagraph>
 	<ul class="text-md mt-2 ml-4 list-inside list-disc md:text-lg">
 		<li>{$_(`common.form.labels.name`)}</li>
 		<li>{$_(`common.form.labels.email`)}</li>
@@ -28,31 +38,27 @@
 		<li>{$_(`page.join.school_label`)}</li>
 	</ul>
 	<h2 class="mt-6 text-2xl font-bold md:text-4xl">{$_('page.privacy.data_handling')}</h2>
-	<p class="text-md mt-4 whitespace-pre-line md:text-lg">
+	<StandardParagraph>
 		{$_(`page.privacy.data_handling_details_1`)}
-	</p>
-	<p class="text-md mt-4 whitespace-pre-line md:text-lg">
+	</StandardParagraph>
+	<StandardParagraph>
 		{$_(`page.privacy.data_handling_details_2`)}
-	</p>
+	</StandardParagraph>
 	<h2 class="mt-6 text-2xl font-bold md:text-4xl">{$_('page.privacy.your_rights')}</h2>
-	<p class="text-md mt-4 whitespace-pre-line md:text-lg">
+	<StandardParagraph>
 		{$_(`page.privacy.your_rights_details`)}
-	</p>
+	</StandardParagraph>
 	<ul class="text-md mt-2 ml-4 list-inside list-disc md:text-lg">
 		<li>{$_(`page.privacy.rights.access`)}</li>
 		<li>{$_(`page.privacy.rights.correction`)}</li>
 		<li>{$_(`page.privacy.rights.deletion`)}</li>
 		<li>{$_(`page.privacy.rights.restriction`)}</li>
 		<li>{$_(`page.privacy.rights.objection`)}</li>
-		<li>{$_(`page.privacy.rights.data_portability`)}</li>
 	</ul>
-	<p class="text-md mt-4 whitespace-pre-line md:text-lg">
+	<StandardParagraph>
 		{$_(`page.privacy.rights.contact`)}
-		<a
-			href="mailto:styret@studenterforpalestina.no"
-			class="text-(--contrast-text-green) underline hover:no-underline"
-		>
+		<StandardLink href="mailto:styret@studenterforpalestina.no" color="green">
 			styret@studenterforpalestina.no
-		</a>
-	</p>
-</div>
+		</StandardLink>
+	</StandardParagraph>
+</PageWrapper>
